39d5bbabc2c38466b6f3cca92a54cf0e67e362f9
Adrian Ngo <adrian.ngo@ibsolution.de>
2020-02-15 17:30:46 +0100

    Add author query

--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,13 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLSchema
+} = graphql;
 
 // Some dummy data
 const books = [
@@ -10,26 +16,53 @@ const books = [
   { name: 'The Wandering Earth', genre:'Sci-Fi', id: '3' }
 ];
 
+const authors = [
+  { name: 'Erich Fromm', country:'Germany', rank:'1', id: '1' },
+  { name: 'Frank Schätzing', country:'Germany', rank:'22', id: '2' },
+  { name: 'Liu Cixin (劉慈欣)', country:'China', rank:'33', id: '3' }
+];
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    rank: { type: GraphQLInt },
+    country: { type: GraphQLString }
   })
 });
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString }},
+      args: { id: { type: GraphQLID }},
       resolve(parent, args){
         // code to get data from database or other source
         return _.find(books, { id: args.id });
       }
+    },
+
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID }},
+      resolve(parent, args){
+        // code to get data from database or other source
+        return _.find(authors, { id: args.id });
+      }
     }
+
   }
 });
 
